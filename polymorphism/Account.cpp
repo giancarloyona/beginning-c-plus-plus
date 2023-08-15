d1177fbf792e753b0c030bb22f8ecb437f430ed5
Giancarlo Angelis <gianyona@gmail.com>
2023-08-14 23:24:45 -0300

    Update Account class

--- polymorphism/Account.cpp
+++ polymorphism/Account.cpp
@@ -1,28 +1,35 @@
 #include "Account.h"
+#include "../exception-handling/IllegalBalanceException.h"
+#include "../exception-handling/InsufficientFundsException.h"
 
 Account::Account(std::string name, double balance)
-    : name{name}, balance{balance} {
+        : name{name}, balance{balance} {
+    if (balance < 0) {
+        throw IllegalBalanceException();
+
+    }
 }
 
 bool Account::deposit(double amount) {
-  if (amount < 0)
-    return false;
-  else {
-    balance += amount;
-    return true;
-  }
+    if (amount < 0)
+        return false;
+    else {
+        balance += amount;
+        return true;
+    }
 }
 
 bool Account::withdraw(double amount) {
-  if (balance - amount >= 0) {
-    balance -= amount;
-    return true;
-  } else
-    return false;
+    if (balance - amount >= 0) {
+        balance -= amount;
+        return true;
+    } else {
+        throw InsufficientFundsException();
+    }
 }
 
-void Account::print(std::ostream &os) const  {
-  os.precision(2);
-  os << std::fixed;
-  os << "[Account: " << name << ": " << balance << "]";
+void Account::print(std::ostream &os) const {
+    os.precision(2);
+    os << std::fixed;
+    os << "[Account: " << name << ": " << balance << "]";
 }
